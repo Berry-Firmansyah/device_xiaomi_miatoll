e9b27b5ffaf8dcb5305496671b9b78924626d5e1
berry <beryart222@gmail.com>
2024-08-10 00:09:43 +0700

    fix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/afterlife_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_miatoll-ap2a-user \
-    afterlife_miatoll-ap2a-userdebug \
-    afterlife_miatoll-ap2a-eng
+    afterlife_miatoll-user \
+    afterlife_miatoll-userdebug \
+    afterlife_miatoll-eng
