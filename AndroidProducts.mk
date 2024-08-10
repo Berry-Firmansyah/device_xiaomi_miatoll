5ea1b4ec4b2362b988d949d96ce2748b3da21fea
berry <beryart222@gmail.com>
2024-08-10 13:34:45 +0700

    fix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_miatoll.mk
+PRODUCT_MAKEFILES := \ 
+$(LOCAL_DIR)/afterlife_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_miatoll-user \
-    afterlife_miatoll-userdebug \
-    afterlife_miatoll-eng
+  afterlife_miatoll-ap2a-user \
+  afterlife_miatoll-ap2a-userdebug \
+  afterlife_miatoll-ap2a-eng
